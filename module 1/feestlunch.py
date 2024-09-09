949204a5732148a8d953fc9d4baadd887623a5c7
Finnemans <99080585@mydavinci.nl>
2024-09-09 11:07:08 +0200

    opdr

---
+++ module 1/feestlunch.py
@@ -0,0 +1,11 @@
+croissantjes = 17
+stokbroden = 2
+bonnen = 3
+
+var1 = croissantjes * 0.39
+var2 = stokbroden * 2.
+var3 = bonnen * 0.50
+
+antwoord = var1 + var2 - var3
+
+print(f"De feestlunch kost je bij de bakker {antwoord} euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!")
---
+++ module 1/speeldag.py
@@ -0,0 +1,10 @@
+deelnemers = 4
+entree = 7.45
+vip = 0.37
+viptijd = 9
+
+vipprijs = vip * viptijd * deelnemers
+entreeprijs = entree * deelnemers
+totaal = vipprijs + entreeprijs
+
+print(f'totaal prijs os {totaal}euro ')
