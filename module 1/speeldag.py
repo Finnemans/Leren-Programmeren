afc9af004e88b8dc32ee05c8cb1925f4600637ad
Finnemans <99080585@mydavinci.nl>
2024-09-09 11:08:11 +0200

    Update speeldag.py

--- module 1/speeldag.py
+++ module 1/speeldag.py
@@ -7,4 +7,4 @@ vipprijs = vip * viptijd * deelnemers
 entreeprijs = entree * deelnemers
 totaal = vipprijs + entreeprijs
 
-print(f'totaal prijs os {totaal}euro ')
+print(f'Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar {totaal} euro')
