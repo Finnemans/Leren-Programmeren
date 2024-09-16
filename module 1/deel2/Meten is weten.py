870219a4fd57aab81e5b31375728c43b9946f0b7
Finnemans <99080585@mydavinci.nl>
2024-09-16 09:18:47 +0200

    meten is weten if statement

---
+++ module 1/deel2/Meten is weten.py
@@ -0,0 +1,9 @@
+vara = int(input('vul hier getal A in: '))
+varb = int(input('vul hier getal B in: '))
+
+if vara > varb:
+    max = vara
+    print(f'a is het grootste getal:{max}')
+else:
+    max = varb
+    print(f'b is het grootste getal:{max}')
