c19507036bd87a58e4a0c00ae19dfeeae05567d5
Finnemans <99080585@mydavinci.nl>
2024-09-16 10:52:26 +0200

    wie is de gastheer

--- module 1/deel2/startTheParty.py
+++ module 1/deel2/startTheParty.py
@@ -1,7 +1,8 @@
 
-gastheer = False
-gasten = False
-drank = False
+naam = input('hoe heet de gastheer: ')
+gastheer = True
+gasten = True
+drank = True
 chips = True
 
 #Een feest moet minimaal gasten of een gastheer hebben.
@@ -11,9 +12,9 @@ start_condition_3 = chips
 start_condition_4 = gasten
 start_condition_5 = gastheer or drank
 start_condition_6 = gastheer or gasten or drank
+startcondition7 = naam == 'finn'
 
-if start_condition_1 and start_condition_2 and start_condition_3 and start_condition_4 and start_condition_5 and start_condition_6:
+if start_condition_1 and start_condition_2 and startcondition7 and start_condition_3 and start_condition_4 and start_condition_5 and start_condition_6:
     print('Start the Party')
 else:
     print('No Party')
-
