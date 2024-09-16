c03d8e99298b13f4160c3eb58af1823b7bc09df2
Finnemans <99080585@mydavinci.nl>
2024-09-16 11:02:11 +0200

    minmax gasten

--- module 1/deel2/startTheParty.py
+++ module 1/deel2/startTheParty.py
@@ -1,7 +1,7 @@
 
 naam = input('hoe heet de gastheer: ')
 gastheer = True
-gasten = True
+gasten = int(input('hoeveel gasten komen er: '))
 drank = True
 chips = True
 
@@ -9,12 +9,13 @@ chips = True
 start_condition_1 = gastheer or gasten
 start_condition_2 = gasten or chips or drank
 start_condition_3 = chips
-start_condition_4 = gasten
+start_condition_4 = gasten >= 4
 start_condition_5 = gastheer or drank
 start_condition_6 = gastheer or gasten or drank
 startcondition7 = naam == 'finn'
+startcondition8 = gasten <= 20
 
-if start_condition_1 and start_condition_2 and startcondition7 and start_condition_3 and start_condition_4 and start_condition_5 and start_condition_6:
+if start_condition_1 and start_condition_2 and startcondition8 and startcondition7 and start_condition_3 and start_condition_4 and start_condition_5 and start_condition_6:
     print('Start the Party')
 else:
     print('No Party')
