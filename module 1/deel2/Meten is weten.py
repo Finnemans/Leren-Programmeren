6679e7a4b7386ccfd5e52cf6f283a2b111d66a7b
Finnemans <99080585@mydavinci.nl>
2024-09-16 09:25:51 +0200

    min max

--- module 1/deel2/Meten is weten.py
+++ module 1/deel2/Meten is weten.py
@@ -3,12 +3,16 @@ varb = int(input('vul hier getal B in: '))
 
 if vara > varb:
     max = vara
+    min = varb
     print(f'a is het grootste getal:{max}')
-# else:
-#     max = varb
-#     print(f'b is het grootste getal:{max}')
 elif vara < varb:
     min = vara
+    max = varb
     print(f'a is het kleinste getal:{min}')
 else:
     print('a en b zijn even groot')
+
+
+
+print(f'het minimum is:{min}')
+print(f'het maximum is:{max}')
