7b2fd83b35954bba38e1beb4921483debef93eb0
Finnemans <99080585@mydavinci.nl>
2024-09-16 09:21:09 +0200

    elif statement

--- module 1/deel2/Meten is weten.py
+++ module 1/deel2/Meten is weten.py
@@ -4,6 +4,9 @@ varb = int(input('vul hier getal B in: '))
 if vara > varb:
     max = vara
     print(f'a is het grootste getal:{max}')
-else:
-    max = varb
-    print(f'b is het grootste getal:{max}')
+# else:
+#     max = varb
+#     print(f'b is het grootste getal:{max}')
+elif vara < varb:
+    min = vara
+    print(f'a is het kleinste getal:{min}')
