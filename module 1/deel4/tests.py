311976217a78c155665a2191b17890d87e0aabae
Finnemans <99080585@mydavinci.nl>
2024-10-21 12:12:23 +0200

    Update tests.py

--- module 1/deel4/tests.py
+++ module 1/deel4/tests.py
@@ -1,19 +1,19 @@
 from meten import gelijk, groot, klein
 from test_lib import test, report
 
-nr1 = int(input('Vul hier getal A in: '))
-nr2 = int(input('Vul hier getal B in: '))
+# nr1 = int(input('Vul hier getal A in: '))
+# nr2 = int(input('Vul hier getal B in: '))
 
-expected = f'{nr1} en {nr2} zijn even groot' #resultaat
-result = gelijk(nr1, nr2) #roep hier je functie aan met 2 dezelfde getallen
+expected = f'{5} en {5} zijn even groot' #resultaat
+result = gelijk(5, 5) #roep hier je functie aan met 2 dezelfde getallen
 test('TEST nr1=nr2', expected, result)
 
-expected = f'Maximum: {nr1} en minimum: {nr2}'#resultaat
-result = groot(nr1, nr2)
+expected = f'Maximum: {10} en minimum: {4}'#resultaat
+result = groot(10, 4)
 test('TEST nr1>nr2', expected, result)
 
-expected = f'Maximum: {nr2} en minimum: {nr1}' #resultaat
-result = klein(nr1, nr2) #roep hier je functie aan  waar nr1 kleiner is dan nr2
+expected = f'Maximum: {7} en minimum: {3}' #resultaat
+result = klein(3, 7) #roep hier je functie aan  waar nr1 kleiner is dan nr2
 test('TEST nr1<nr2', expected, result)
 
 if __name__ == "__main__":
