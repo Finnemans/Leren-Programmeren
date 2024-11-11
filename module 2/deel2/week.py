76b3fb4f226cc89dcda84e589a21125c2d49d0c7
Finnemans <99080585@mydavinci.nl>
2024-11-11 09:51:20 +0100

    Update week.py

--- module 2/deel2/week.py
+++ module 2/deel2/week.py
@@ -5,18 +5,25 @@ for dag in week:
     print(f'- {dag}')
 print(' ')
 
-print(f'De weekend dagen zijn: {week[5]} & {week[6]}.')
+print(f'De weekend dagen zijn:')
+for dag in week[-2:]:
+    print(f'- {dag}')
 print(' ')
 
-print(f'De werkdagen zijn: {week[0]}, {week[1]}, {week[2]}, {week[3]} & {week[4]}.')
+print(f'De werkdagen zijn:')
+for dag in week[:5]:
+    print(f'- {dag}')
 print(' ')
 
-print(f'Alle dagen van de week in omgekeerde volgorde zijn: {week[6]} -> {week[5]} -> {week[4]} -> {week[3]} -> {week[2]} -> {week[1]} -> {week[0]}')
-print(' ')
+print(f'Alle dagen van de week in omgekeerde volgorde zijn:')
+for dag in week[::-1]:
+   print(f'- {dag}')
 
 print('De werkdagen in omgekeerde volgorde zijn:')
 for dag in week[4::-1]:
     print(f'- {dag}')
 print(' ')
 
-print(f'De weekend dagen in omgekeerde volgorde zijn: {week[6]} & {week[5]}.')
+print(f'De weekend dagen in omgekeerde volgorde zijn:')
+for dag in week[-1:-3:-1]:
+    print(f'- {dag}')
