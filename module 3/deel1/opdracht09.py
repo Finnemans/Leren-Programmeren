fde727f21f3e7850dcfe7508231556ed2efd3f17
Finnemans <99080585@mydavinci.nl>
2025-01-06 10:07:17 +0100

    Update opdracht09.py

--- module 3/deel1/opdracht09.py
+++ module 3/deel1/opdracht09.py
@@ -1,4 +1,8 @@
 from fruitmand import fruitmand
 
-for fruit in fruitmand[::-1]:
-    print(fruit['name'])
+for fruit in fruitmand:
+    if fruit['name'] == 'druif':
+        fruitmand.remove(fruit)
+
+for fruit in fruitmand:
+    print(fruit['color'])
