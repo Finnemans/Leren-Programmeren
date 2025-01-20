eaf32b662cfa88d20b1f6cab114c0620009903d3
Finnemans <99080585@mydavinci.nl>
2025-01-20 11:45:39 +0100

    coins

--- module 2/deel3/coins.py
+++ module 2/deel3/coins.py
@@ -2,7 +2,9 @@
 # number of student: 99080585
 # purpose of program: wisselgeld
 # structure of program: 
-coinValues = [1000, 500, 200, 100, 50, 20, 10]  # die euros naar centen
+coinValues = [500, 200, 100, 50, 20, 10]  # die euros naar centen
+munten = []
+aantal = []
 
 toPay = int(float(input('Amount to pay: ')) * 100)  # te betalen van euros naar centen
 paid = int(float(input('Paid amount: ')) * 100)  # betaalde van euros naar centen
@@ -15,14 +17,23 @@ while change > 0 and len(coinValues) > 0:  # herhaal als er nog wisselgeld en no
     if nrCoins > 0:  # alleen als er 1 munt van die waarde is
         if coinValue >= 100:  # meer dan 100cent
             print(f"Return maximal {nrCoins}x of {coinValue // 100} euros!") # cent weer als euro
-            nrCoinsReturned = int(input(f'How many times of {coinValue // 100} euros did you return? '))
+            nrCoinsReturned = int(input(f'How many coins of {coinValue // 100} euros did you return? '))
         else:
             print(f'Return maximal {nrCoins} coins of {coinValue} cents!')
             nrCoinsReturned = int(input(f'How many coins of {coinValue} cents did you return? '))
         
         change -= nrCoinsReturned * coinValue  # nieuw overig te betalen
+        
+        munten.append(coinValue)  # Sla waarde in centen op
+        aantal.append(nrCoinsReturned) # sla aantal munten op
 
 if change > 0: # als na de list nog steeds geld over is
     print(f'Change not returned: {change} cents') # hoeveel wisselgeld nog te betalen
 else:
     print('Done!') # klaar
+
+for item1, item2 in zip(munten, aantal):
+    if item1 >= 100:  # Voor euro's
+        print(f'{item2}x {item1 // 100} euro')
+    else:  # Voor centen
+        print(f'{item2}x {item1} cent')
--- module 3/deel1/opdracht09.py
+++ module 3/deel1/opdracht09.py
@@ -1,8 +1,10 @@
 from fruitmand import fruitmand
-
+kleuren = []
 for fruit in fruitmand:
     if fruit['name'] == 'druif':
         fruitmand.remove(fruit)
 
 for fruit in fruitmand:
-    print(fruit['color'])
+    if (fruit['color']) not in kleuren:
+        kleuren.append(fruit['color'])
+print(kleuren)
