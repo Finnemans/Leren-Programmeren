aa2a28022cf4d779b089e7e7f20551bc92e80357
Finnemans <99080585@mydavinci.nl>
2025-03-05 11:28:45 +0100

    Create printen2.py

---
+++ module 4/deel 1/naam woonplaats leeftijd/printen2.py
@@ -0,0 +1,16 @@
+from termcolor import colored
+from functie import verzamel_personen
+
+def check_volwassen(leeftijd):
+    if leeftijd >= 18:
+        return colored(f"al {leeftijd} jaar", "red")
+    else:
+        return colored("nog niet", "red")
+
+personen_lijst = verzamel_personen()
+
+for persoon in personen_lijst:
+    naam = colored(persoon['name'].capitalize(), "green")
+    stad = colored(persoon['city'].capitalize(), "yellow")
+
+    print(f"In {stad} woont {naam}, die {check_volwassen(persoon['age'])} volwassen is.")
