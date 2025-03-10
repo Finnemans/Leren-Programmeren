7f328c7c8431d09566a1205a417f879a65721f16
Finnemans <99080585@mydavinci.nl>
2025-03-10 11:16:50 +0100

    rekenmachine

---
+++ module 4/deel 2/rekenmachine/functions.py
@@ -0,0 +1,13 @@
+def addition(number1, number2):
+    return number1 + number2
+
+def subtraction(number1, number2):
+    return number1 - number2
+
+def multiplication(number1, number2):
+    return number1 * number2
+
+def division(number1, number2):
+    if number2 == 0:
+        return "Error: Kan niet delen door nul"
+    return number1 / number2
